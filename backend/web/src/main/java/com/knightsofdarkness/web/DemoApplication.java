0a423b82f8efb84605d054aba6a2fa1fce210987
lmerynda <lmerynda@gmail.com>
2024-05-31 21:28:18 +0000

    load some data on startup

--- backend/web/src/main/java/com/knightsofdarkness/web/DemoApplication.java
+++ backend/web/src/main/java/com/knightsofdarkness/web/DemoApplication.java
@@ -1,19 +1,98 @@
 package com.knightsofdarkness.web;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan(basePackages =
-{ "com.knightsofdarkness.*" })
+		{ "com.knightsofdarkness.*" })
 @EntityScan(basePackages =
-{ "com.knightsofdarkness.*" })
+		{ "com.knightsofdarkness.*" })
 public class DemoApplication {
 	public static void main(String[] args)
 	{
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate()
+	{
+		return new RestTemplate();
+	}
+
+	@Bean
+	public HttpHeaders httpHeaders()
+	{
+		return new HttpHeaders();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate, HttpHeaders headers) throws Exception
+	{
+		return args ->
+		{
+			headers.setContentType(MediaType.APPLICATION_JSON);
+
+			// POST request to /kingdom
+			String kingdomPayload = """
+					{
+						"name": "uprzejmy"
+					}
+					""";
+			HttpEntity<String> kingdomEntity = new HttpEntity<>(kingdomPayload, headers);
+			restTemplate.postForObject("http://localhost:8080/kingdom/", kingdomEntity, String.class);
+
+			// POST request to /market/add
+			String marketPayload = """
+					{
+						"kingdomName": "uprzejmy",
+						"resource": "food",
+						"count": 100,
+						"price": 30
+					}
+					""";
+			HttpEntity<String> marketEntity = new HttpEntity<>(marketPayload, headers);
+			restTemplate.postForObject("http://localhost:8080/market/add", marketEntity, String.class);
+
+			// POST request to /market_fixtures
+			String marketFixturesPayload = """
+					[
+						{
+							"kingdomName": "uprzejmy",
+							"resource": "food",
+							"count": 10000,
+							"price": 10
+						},
+						{
+							"kingdomName": "uprzejmy",
+							"resource": "food",
+							"count": 20000,
+							"price": 30
+						},
+						{
+							"kingdomName": "uprzejmy",
+							"resource": "iron",
+							"count": 10000,
+							"price": 50
+						},
+						{
+							"kingdomName": "uprzejmy",
+							"resource": "iron",
+							"count": 15000,
+							"price": 70
+						}
+					]
+					""";
+			HttpEntity<String> marketFixturesEntity = new HttpEntity<>(marketFixturesPayload, headers);
+			restTemplate.postForObject("http://localhost:8080/market_fixtures", marketFixturesEntity, String.class);
+		};
+	}
 }
