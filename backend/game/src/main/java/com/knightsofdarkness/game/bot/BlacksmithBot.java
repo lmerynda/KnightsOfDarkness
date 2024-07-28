5c067b049fe2e5b785185b63c07f4a592831661c
lmerynda <lmerynda@gmail.com>
2024-07-28 06:23:45 +0000

    always post some tools to the market

--- backend/game/src/main/java/com/knightsofdarkness/game/bot/BlacksmithBot.java
+++ backend/game/src/main/java/com/knightsofdarkness/game/bot/BlacksmithBot.java
@@ -1,5 +1,8 @@
 package com.knightsofdarkness.game.bot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.knightsofdarkness.game.kingdom.BuildingName;
 import com.knightsofdarkness.game.kingdom.Kingdom;
 import com.knightsofdarkness.game.kingdom.ResourceName;
@@ -8,6 +11,7 @@ import com.knightsofdarkness.game.market.IMarket;
 import com.knightsofdarkness.game.market.MarketResource;
 
 public class BlacksmithBot implements Bot {
+    private static final Logger log = LoggerFactory.getLogger(BlacksmithBot.class);
     private final Kingdom kingdom;
     private final IMarket market;
     private final double builderToSpecialistRatio = 0.07;
@@ -23,6 +27,8 @@ public class BlacksmithBot implements Bot {
     public boolean doAllActions()
     {
         BotFunctions.withdrawAllOffers(kingdom, market);
+        // always put something on the market to have any income for supplies
+        postToolsOffer(0.2);
 
         boolean hasAnythingHappened = true;
         do
@@ -30,7 +36,7 @@ public class BlacksmithBot implements Bot {
             hasAnythingHappened = doActionCycle();
         } while (hasAnythingHappened);
 
-        postToolsOffer();
+        postToolsOffer(1.0);
 
         return hasAnythingHappened;
     }
@@ -53,13 +59,13 @@ public class BlacksmithBot implements Bot {
         return hasAnythingHappen;
     }
 
-    private int postToolsOffer()
+    private int postToolsOffer(double percentage)
     {
         var toolsAmount = kingdom.getResources().getCount(ResourceName.tools);
 
         if (toolsAmount > 0)
         {
-            market.addOffer(kingdom, MarketResource.tools, toolsAmount, 140);
+            market.addOffer(kingdom, MarketResource.tools, (int)(toolsAmount * percentage), 140);
         }
 
         return toolsAmount;
@@ -74,8 +80,8 @@ public class BlacksmithBot implements Bot {
     @Override
     public String getKingdomInfo()
     {
-        return String.format("[%s] passed turn, land: %d, houses: %d, workshops: %d, gold: %d, food: %d, iron: %d, tools: %d", kingdom.getName(), kingdom.getResources().getCount(ResourceName.land),
-                kingdom.getBuildings().getCount(BuildingName.house),
+        return String.format("[%s] land: %d, houses: %d, workshops: %d, gold: %d, food: %d, iron: %d, tools: %d", kingdom.getName(), kingdom.getResources().getCount(ResourceName.land),
+                        kingdom.getBuildings().getCount(BuildingName.house),
                 kingdom.getBuildings().getCount(BuildingName.workshop), kingdom.getResources().getCount(ResourceName.gold), kingdom.getResources().getCount(ResourceName.food), kingdom.getResources().getCount(ResourceName.iron),
                 kingdom.getResources().getCount(ResourceName.tools));
     }
@@ -99,6 +105,8 @@ public class BlacksmithBot implements Bot {
         var nourishmentFactor = 1.0; // Assume everyone is fed
         var ironUpkeep = kingdom.getIronUpkeep(nourishmentFactor);
         var ironAmount = kingdom.getResources().getCount(ResourceName.iron);
-        return ironAmount / ironUpkeep >= 0.8d;
+        int ironReserve = ironAmount / ironUpkeep;
+        log.info("[{}] iron reserve {}", kingdom.getName(), ironReserve);
+        return ironReserve >= 0.8d;
     }
 }
