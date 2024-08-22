89f33b7fc2385891443af352b04d27ea78ffff4a
uprzejmy <lmerynda@gmail.com>
2024-08-22 16:59:24 -0500

    move sonar properties around

--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -1,2 +1,10 @@
 rootProject.name = "knights-of-darkness"
 include("game", "storage", "web", "common")
+
+sonar {
+  properties {
+    property("sonar.projectKey", "Uprzejmy_KnightsOfDarkness")
+    property("sonar.organization", "uprzejmy")
+    property("sonar.host.url", "https://sonarcloud.io")
+  }
+}
