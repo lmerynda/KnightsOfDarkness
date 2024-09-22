8a6f20928ee51bed79ae6267428a8d72dd9099e5
lmerynda <lmerynda@gmail.com>
2024-09-22 05:54:35 +0000

    logout functionality

--- browser/src/KingdomToolbar.tsx
+++ browser/src/KingdomToolbar.tsx
@@ -1,20 +1,35 @@
 import React from "react";
 import KingdomResourcesView from "./components/KingdomResources";
 import { KingdomDetails, KingdomResources } from "./GameTypes";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 export type KingdomToolbarProps = {
   kingdomName: string;
   kingdomResources: KingdomResources;
   kingdomDetails: KingdomDetails;
 };
-
 const KingdomToolbar: React.FC<KingdomToolbarProps> = ({ kingdomName, kingdomResources, kingdomDetails }) => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("authToken");
+    navigate("/homepage");
+  };
+
   return (
     <div>
-      <h2>Kingdom {kingdomName}</h2>
+      <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+        <h2>Kingdom {kingdomName}</h2>
+        <div>
+          <Button variant="contained" onClick={() => handleLogout()}>
+            Logout
+          </Button>
+        </div>
+      </div>
       <KingdomResourcesView kingdomResources={kingdomResources} kingdomDetails={kingdomDetails} />
     </div>
   );
-};
+}; 
 
 export default KingdomToolbar;
