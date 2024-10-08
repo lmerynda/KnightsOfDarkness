0e1834264febd9ece48f705eb5cdeeb420b8d555
lmerynda <lmerynda@gmail.com>
2024-10-08 15:44:20 -0500

    Carriers on the move (#37)

--- backend/game/src/test/java/com/knightsofdarkness/game/utils/KingdomBuilder.java
+++ backend/game/src/test/java/com/knightsofdarkness/game/utils/KingdomBuilder.java
@@ -9,6 +9,7 @@ import com.knightsofdarkness.common.kingdom.UnitName;
 import com.knightsofdarkness.game.Game;
 import com.knightsofdarkness.game.kingdom.Kingdom;
 import com.knightsofdarkness.game.kingdom.KingdomBuildings;
+import com.knightsofdarkness.game.kingdom.KingdomCarriersOnTheMove;
 import com.knightsofdarkness.game.kingdom.KingdomResources;
 import com.knightsofdarkness.game.kingdom.KingdomSpecialBuilding;
 import com.knightsofdarkness.game.kingdom.KingdomUnits;
@@ -100,6 +101,7 @@ public class KingdomBuilder {
 
     public Kingdom build()
     {
-        return new Kingdom(name, game.getConfig(), new KingdomResources(resources), new KingdomBuildings(buildings), new ArrayList<KingdomSpecialBuilding>(), new KingdomUnits(units), new KingdomTurnReport());
+        return new Kingdom(name, game.getConfig(), new KingdomResources(resources), new KingdomBuildings(buildings), new ArrayList<KingdomSpecialBuilding>(), new ArrayList<KingdomCarriersOnTheMove>(), new KingdomUnits(units),
+                        new KingdomTurnReport());
     }
 }
