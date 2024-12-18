821fe1388e91e1a8a122b1eec8086b025b8ecc23
lmerynda <lmerynda@gmail.com>
2024-12-18 06:22:43 +0000

    defense salvo in attack resolve

--- backend/common/src/main/java/com/knightsofdarkness/common/kingdom/UnitsMapDto.java
+++ backend/common/src/main/java/com/knightsofdarkness/common/kingdom/UnitsMapDto.java
@@ -34,6 +34,11 @@ public class UnitsMapDto {
         units.put(name, count);
     }
 
+    public void subtractCount(UnitName name, int count)
+    {
+        units.put(name, units.get(name) - count);
+    }
+
     public Map<UnitName, Integer> getUnits()
     {
         return units;
@@ -42,6 +47,24 @@ public class UnitsMapDto {
     public int countAll()
     {
         return units.values().stream().mapToInt(Integer::intValue).sum();
+    }
+
+    public int countMilitary()
+    {
+        return UnitName.getMilitaryUnits().stream().mapToInt(units::get).sum();
+    }
+
+    public Map<UnitName, Double> getMilitaryUnitsRatios()
+    {
+        var totalMilitaryCount = countMilitary();
+        var unitRatios = new EnumMap<UnitName, Double>(UnitName.class);
+        for (var unit : UnitName.getMilitaryUnits())
+        {
+            var count = units.get(unit);
+            unitRatios.put(unit, (double) count / totalMilitaryCount);
+        }
+
+        return unitRatios;
     }
 
     public void setUnit(String key, int value)
--- backend/game/src/main/java/com/knightsofdarkness/game/interactions/KingdomInteractor.java
+++ backend/game/src/main/java/com/knightsofdarkness/game/interactions/KingdomInteractor.java
@@ -3,6 +3,7 @@ package com.knightsofdarkness.game.interactions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.knightsofdarkness.common.kingdom.UnitName;
 import com.knightsofdarkness.common.market.MarketResource;
 import com.knightsofdarkness.game.kingdom.Kingdom;
 import com.knightsofdarkness.game.kingdom.KingdomOngoingAttack;
@@ -33,9 +34,52 @@ public class KingdomInteractor implements IKingdomInteractor {
     }
 
     @Override
-    public void resolveAttack(Kingdom kingdom, KingdomOngoingAttack attack)
+    public void resolveAttack(Kingdom attackerKingdom, KingdomOngoingAttack attack)
     {
-        log.info("[KingdomInteractor] Resolving attack from {} on {}", kingdom.getName(), attack.getTargetKingdomName());
-        // TODO complete implementation
+        log.info("[KingdomInteractor] Resolving attack from {} on {}", attackerKingdom.getName(), attack.getTargetKingdomName());
+
+        var maybeDefendantKingdom = kingdomRepository.getKingdomByName(attack.getTargetKingdomName());
+        if (maybeDefendantKingdom.isEmpty())
+        {
+            log.warn("[KingdomInteractor] Kingdom with name {} not found", attack.getTargetKingdomName());
+            return;
+        }
+
+        var defendantKingdom = maybeDefendantKingdom.get();
+
+        processDefenseSalvo(attackerKingdom, defendantKingdom, attack);
+        processAttackSalvo(attackerKingdom, defendantKingdom, attack);
+        processMelee(attackerKingdom, defendantKingdom, attack);
+    }
+
+    private void processDefenseSalvo(Kingdom attackerKingdom, Kingdom defenderKingdom, KingdomOngoingAttack attack)
+    {
+        var defenderBowmenCount = defenderKingdom.getUnits().getAvailableCount(UnitName.bowman);
+        var attackerUnits = attack.getUnits();
+        var attackerUnitsRatios = attack.getUnits().getMilitaryUnitsRatios();
+
+        var bowmenHittingBowmen = (int) Math.ceil(attackerUnitsRatios.get(UnitName.bowman) * defenderBowmenCount);
+        var bowmenHittingInfantry = (int) Math.ceil(attackerUnitsRatios.get(UnitName.infantry) * defenderBowmenCount);
+        var bowmenHittingCavarly = (int) Math.ceil(attackerUnitsRatios.get(UnitName.cavalry) * defenderBowmenCount);
+
+        var killedAttackingBowmen = Math.min(bowmenHittingBowmen / 2, attackerUnits.getCount(UnitName.bowman));
+        var killedAttackingInfantry = Math.min(bowmenHittingInfantry / 3, attackerUnits.getCount(UnitName.infantry));
+        var killedAttackingCavalry = Math.min(bowmenHittingCavarly / 4, attackerUnits.getCount(UnitName.cavalry));
+
+        attackerUnits.subtractCount(UnitName.bowman, killedAttackingBowmen);
+        attackerUnits.subtractCount(UnitName.infantry, killedAttackingInfantry);
+        attackerUnits.subtractCount(UnitName.cavalry, killedAttackingCavalry);
+    }
+
+    private void processAttackSalvo(Kingdom attackerKingdom, Kingdom defendantKingdom, KingdomOngoingAttack attack)
+    {
+        // XXX Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'processAttackSalvo'");
+    }
+
+    private void processMelee(Kingdom attackerKingdom, Kingdom defendantKingdom, KingdomOngoingAttack attack)
+    {
+        // XXX Auto-generated method stub
+        // throw new UnsupportedOperationException("Unimplemented method 'processMelee'");
     }
 }
