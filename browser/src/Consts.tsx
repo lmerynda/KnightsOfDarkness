9097b2ef9521f93a2f792c396f76c37a335698d4
lmerynda <lmerynda@gmail.com>
2025-01-13 07:22:43 +0000

    prod/dev frontend api const

--- browser/src/Consts.tsx
+++ browser/src/Consts.tsx
@@ -1,4 +1,4 @@
-export const GAME_API = "http://localhost:8081";
+export const GAME_API = import.meta.env.VITE_GAME_API;
 export const kingdomRefreshInterval = 5000;
 export const marketRefreshInterval = 5000;
 export const notificationsRefreshInterval = 5000;
